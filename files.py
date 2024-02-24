c773aeb622e88aeb7e768dfc2c95f7eebeb267e9
Yannick <132798545+Kunnak@users.noreply.github.com>
2024-02-24 13:10:48 +0100

    Add files via upload

---
+++ files.py
@@ -0,0 +1,38 @@
+#######################################################################################################################
+# IMPORTS
+#######################################################################################################################
+
+import datetime
+import welcome
+
+#######################################################################################################################
+# VARIABLES
+#######################################################################################################################
+
+system_date = datetime.date.today()
+system_time = datetime.datetime.now()
+time = system_time.strftime("%H:%M")
+
+output_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+nickname_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+log_nickname = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+backup_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+
+output_backup = '[BACKUP]'
+output_nickname = '[NEW NICKNAME]'
+
+bool_backup = False
+
+backup_done = 'SUCCESS'
+backup_fail = 'FAILED'
+
+nickname = welcome.nickname
+
+#######################################################################################################################
+# CSV-DATA
+#######################################################################################################################
+
+csv_nicknames = "nicknames.csv"
+csv_highscore = "highscore.csv"
+csv_logfile = "logfile.csv"
+csv_nicknames_backup = "nicknames_backup.csv"
---
+++ functions.py
@@ -0,0 +1,87 @@
+#######################################################################################################################
+# IMPORTS
+#######################################################################################################################
+
+import csv
+import files
+
+#######################################################################################################################
+# VARIABLES
+#######################################################################################################################
+
+output_time = files.output_time
+bool_backup = False
+bool_backup_logfile = False
+trenner = "#######################################################################################################################"
+
+#######################################################################################################################
+# FUNCTIONS
+#######################################################################################################################
+
+
+def create_backup():
+    global bool_backup, bool_backup_logfile
+
+    if bool_backup:
+
+        with open(files.csv_nicknames_backup, mode='a', newline='') as file2:
+            writer = csv.writer(file2)
+
+            with open(files.csv_nicknames, mode='r', newline='') as file1:
+                reader = csv.reader(file1)
+
+                writer.writerow([trenner])
+                writer.writerow([' ', ' ', ' ',files.backup_time])
+
+                for row in reader:
+                    writer.writerow(row)
+
+            bool_backup_logfile = True
+
+        if bool_backup_logfile:
+
+            with open(files.csv_logfile, mode='a', newline='') as logfile:
+                writer_log = csv.writer(logfile)
+
+                output_time.append(files.output_backup)
+                output_time.append(files.backup_done)
+
+                writer_log.writerow(output_time)
+                output_time.clear()
+
+                bool_backup = False
+                bool_backup_logfile = False
+
+    else:
+
+        with open(files.csv_logfile, mode='a', newline='') as logfile:
+            writer = csv.writer(logfile)
+
+            output_time.append(files.output_backup)
+            output_time.append(files.backup_fail)
+
+            writer.writerow(output_time)
+            output_time.clear()
+
+            bool_backup = False
+
+
+def save_nickname():
+    global output_time, bool_backup
+
+    with open(files.csv_nicknames, mode='a', newline='') as file:
+        writer = csv.writer(file)
+
+        files.nickname_time.append(files.welcome.nickname)
+
+        writer.writerow(files.nickname_time)
+
+        bool_backup = True
+
+    with open(files.csv_logfile, mode='a', newline='') as file:
+        writer = csv.writer(file)
+
+        files.log_nickname.append(files.output_nickname)
+        files.log_nickname.append(files.welcome.nickname)
+
+        writer.writerow(files.log_nickname)

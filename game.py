4f2f32e38c79d3fd9030ae1e38b63249cc0f7cab
Yannick <132798545+Kunnak@users.noreply.github.com>
2024-02-27 01:16:04 +0100

    Update game.py

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ WHITE = (255, 255, 255)
 punkte = 0
 speed = 3
 
+
 class Rectangle:
     def __init__(self, x, y, speed):
         self.x = x
@@ -36,9 +37,11 @@ class Rectangle:
     def is_clicked(self, pos):
         return self.x <= pos[0] <= self.x + self.width and self.y <= pos[1] <= self.y + self.height
 
+
 def spawn_rectangle():
     random_int = random.randint(1, 6)
     return (random_int * 100) - 100
+
 
 rectangles = []
 
@@ -47,36 +50,40 @@ for i in range(5):
 
 running = True
 
-while running:
-    screen.fill(WHITE)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def game_loop():
+    global running, speed
+    while running:
+        screen.fill(WHITE)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                for rectangle in rectangles[:]:
-                    if rectangle.is_clicked(event.pos) and rectangle.y > 20:
-                        rectangles.remove(rectangle)
-                        speed = speed + 0.05
-                        rectangle.speed = speed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
 
-    for rectangle in rectangles[:]:
-        rectangle.move()
-        rectangle.draw(screen)
-        if rectangle.y >= height:
-            rectangles.remove(rectangle)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+
+                    for rectangle in rectangles[:]:
+                        if rectangle.is_clicked(event.pos) and rectangle.y > 20:
+                            rectangles.remove(rectangle)
+                            speed = speed + 0.05
+                            rectangle.speed = speed
+
+        for rectangle in rectangles[:]:
+            rectangle.move()
+            rectangle.draw(screen)
+            if rectangle.y >= height:
+                rectangles.remove(rectangle)
+                rectangles.append(Rectangle(spawn_rectangle(), -200, speed))
+
+        if rectangle.y >= -24:
             rectangles.append(Rectangle(spawn_rectangle(), -200, speed))
 
-    if rectangle.y >= -25:
-        rectangles.append(Rectangle(spawn_rectangle(), -200, speed))
+        clock.tick(FPS)
+        pygame.display.update()
 
-    clock.tick(FPS)
-    pygame.display.update()
-
-pygame.quit()
+    pygame.quit()

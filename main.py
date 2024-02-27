632b36a66ef8bf1d91ee681d7b369bc645e966df
Yannick <132798545+Kunnak@users.noreply.github.com>
2024-02-27 01:23:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import functions
 # PROGRAM
 #######################################################################################################################
 
+game.game_loop()
 welcome.welcome_dialog()
 functions.save_nickname()
 functions.create_backup()

641f2bb4ce16203b8339971f1b4de1c95d93806c
Yannick <132798545+Kunnak@users.noreply.github.com>
2024-03-16 13:22:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,20 @@
 
 import welcome
 import functions
+import game
 
 #######################################################################################################################
 # PROGRAM
 #######################################################################################################################
 
-game.game_loop()
 welcome.welcome_dialog()
-functions.save_nickname()
-functions.create_backup()
+functions.check_nickname_duplicate()
+if functions.bool_nickname_duplicate:
+    functions.save_nickname()
+functions.create_backup_nicknames()
+game.game_loop()
+functions.output_score()
+functions.insert_score()
+functions.output_highscore()
+
+exit()

4d4ef150af0df83264a46d1bf614c274d2bb2cbd
Yannick <132798545+Kunnak@users.noreply.github.com>
2024-03-16 13:19:35 +0100

    Update files.py

--- files.py
+++ files.py
@@ -14,9 +14,13 @@ system_time = datetime.datetime.now()
 time = system_time.strftime("%H:%M")
 
 output_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+
 nickname_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
 log_nickname = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+
 backup_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
+
+highscore_time = ['[TIME: ' + str(system_date) + ' | ' + str(time) + ']']
 
 output_backup = '[BACKUP]'
 output_nickname = '[NEW NICKNAME]'
@@ -24,7 +28,7 @@ output_nickname = '[NEW NICKNAME]'
 bool_backup = False
 
 backup_done = 'SUCCESS'
-backup_fail = 'FAILED'
+backup_fail = 'NO NEED'
 
 nickname = welcome.nickname
 
